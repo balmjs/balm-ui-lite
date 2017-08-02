f63ea6ce48bd7373ab05de11be1c87d6b68888b0
Elf-mousE <ifmouz@gmail.com>
2017-08-02 21:58:49 +0800

    update

--- docs/scripts/views/divider.vue
+++ docs/scripts/views/divider.vue
@@ -2,75 +2,103 @@
   <div class="page--divider">
     <div class="component-title">
       <h3>Divider</h3>
-      <p>Some guys like to use dividers， so we made this component.</p>
+      <p>Some guys like to use dividers, so we made this component.</p>
     </div>
 
-    <div class="snippet-group w320">
-      <ui-divider>divider</ui-divider>
+    <div class="snippet-group">
+      <div class="snippet-header">
+        <div class="snippet-demos">
+          <div class="snippet-demo-padding"></div>
+          <div class="snippet-demo">
+            <div class="w320">
+              <ui-divider>divider</ui-divider>
+            </div>
+          </div>
+          <div class="snippet-demo-padding"></div>
+        </div>
+      </div>
+      <ui-markdown :text="code[0]"></ui-markdown>
     </div>
-
-    <ui-markdown :text="code[0]"></ui-markdown>
 
     <ui-divider>diiiiiiiiiiivider!</ui-divider>
 
     <p>We even made a vertical divider.</p>
     <div class="snippet-group">
-      <div class="demo-divider-container">
-        <div class="a"></div>
-        <ui-divider type="vertical">or</ui-divider>
-        <div class="b"></div>
+      <div class="snippet-header">
+        <div class="snippet-demos">
+          <div class="snippet-demo-padding"></div>
+          <div class="snippet-demo">
+            <div class="demo-divider-container">
+              <div class="a"></div>
+              <ui-divider type="vertical">or</ui-divider>
+              <div class="b"></div>
+            </div>
+          </div>
+          <div class="snippet-demo-padding"></div>
+        </div>
       </div>
+      <ui-markdown :text="code[1]"></ui-markdown>
     </div>
-    <ui-markdown :text="code[1]"></ui-markdown>
 
     <ui-divider>diiiiiiiiiiivider!</ui-divider>
 
     <p>Sometimes you may want to change the color of the dividing line, there is a  simple way here:</p>
 
-    <div class="snippet-group w320 colored">
-      <ui-divider>divider</ui-divider>
+    <div class="snippet-group">
+      <div class="snippet-header">
+        <div class="snippet-demos">
+          <div class="snippet-demo-padding"></div>
+          <div class="snippet-demo">
+            <div class="w320 colored">
+              <ui-divider>divider</ui-divider>
+            </div>
+          </div>
+          <div class="snippet-demo-padding"></div>
+        </div>
+      </div>
+      <ui-markdown :text="code[2]"></ui-markdown>
     </div>
-
-    <ui-markdown :text="code[2]"></ui-markdown>
 
     <ui-apidoc name="divider"></ui-apidoc>
   </div>
 </template>
-<style scoped>
 
-  .snippet-group.w320 {
-    width: 320px;
-  }
+<script>
+import snippets from '../mixins/snippets';
 
-  .snippet-group.colored {
-    color: rgb(255,64,129);
+export default {
+  mixins: [snippets],
+  created() {
+    this.showCode('divider', 3);
   }
-
-  .demo-divider-container {
-    height: 100px;
-    width: 280px;
-    padding: 10px;
-    display: flex;
-    justify-content: space-between;
-    border: 1px solid currentColor;
-  }
-  .demo-divider-container .a, .demo-divider-container .b {
-    height: 80px;
-    width: 80px;
-    background-color: rgb(63,81,181);
-  }
-
-  .demo-divider-container .b {
-    background-color: rgb(255,64,129);
-  }
-</style>
-<script type="text/babel">
-  import snippets from '../mixins/snippets';
-
-  export default {
-    mixins: [snippets],
-    created() {
-      this.showCode('divider', 3);
-    },
-  }
+};
 </script>
+
+<style scoped>
+.w320 {
+  width: 320px;
+}
+
+.w320.colored {
+  color: rgb(255,64,129);
+}
+
+.demo-divider-container {
+  height: 100px;
+  width: 280px;
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid currentColor;
+}
+
+.demo-divider-container .a, .demo-divider-container .b {
+  height: 80px;
+  width: 80px;
+  background-color: rgb(63,81,181);
+}
+
+.demo-divider-container .b {
+  background-color: rgb(255,64,129);
+}
+</style>
--- src/scripts/components/common/divider.vue
+++ src/scripts/components/common/divider.vue
@@ -6,7 +6,7 @@
   </div>
 </template>
 
-<script type="text/babel">
+<script>
 import {isNumber, isString} from '../../helpers';
 
 const typeArr = ['horizontal', 'vertical'];
@@ -24,74 +24,20 @@ export default {
     }
   },
   computed: {
-    className(){
+    className() {
       let type = this.type;
 
       if(isNumber(type)){
         return typeArr[type] || 'horizontal';
       }
 
-      if(isString(type)){
-        if(type.length === 1){
+      if (isString(type)) {
+        if (type.length === 1) {
           return typeObj[type.toLowerCase()] || '';
         }
         return type;
       }
-
     }
   }
 };
 </script>
-
-<style scoped>
-.mdl-divider {
-  display: flex;
-}
-
-.mdl-divider::before, .mdl-divider::after {
-  content: '';
-  display: block;
-}
-
-.mdl-divider span {
-  display: inline-block;
-  flex-shrink: 0;
-  font-size: 18px;
-  text-transform: uppercase;
-}
-
-.mdl-divider.horizontal {
-  width: 100%;
-  align-items: center;
-  justify-content: space-between;
-
-}
-
-.mdl-divider.horizontal::before, .mdl-divider.horizontal::after {
-  height: 1px;
-  width: 50%;
-  border-bottom: 1px solid currentColor;
-}
-
-.mdl-divider.horizontal span {
-  padding: .5em 1em;
-}
-
-.mdl-divider.vertical {
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  width: 2em;
-
-}
-
-.mdl-divider.vertical::before, .mdl-divider.vertical::after {
-  height: 50%;
-  width: 1px;
-  border-left: 1px solid currentColor;
-}
-
-.mdl-divider.vertical span {
-  padding: .5em;
-}
-</style>
