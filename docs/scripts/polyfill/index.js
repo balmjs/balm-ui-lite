840c3ef40405419b4f23dcc9693f5cff2e1af99f
Elf-mousE <ifmouz@gmail.com>
2017-12-17 19:37:40 +0800

    0.6.0 beta.7

--- docs/scripts/polyfill/index.js
+++ docs/scripts/polyfill/index.js
@@ -2,8 +2,10 @@
 if (typeof(NodeList.prototype.forEach) !== typeof(alert)) {
   NodeList.prototype.forEach = Array.prototype.forEach;
 }
-
-import 'core-js/shim'; // for IE
-// import 'classlist.js'; // for IE9-
-// import 'selectivizr'; // for IE9-
-// import './rAF'; // for IE9-
+// for IE
+import 'core-js/shim';
+// for IE9-
+// import 'classlist.js';
+// import 'matchmedia-polyfill';
+// import 'matchmedia-polyfill/matchMedia.addListener';
+// import './rAF';
