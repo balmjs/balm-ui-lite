3a2094aff5f1f49dddd9cb55f3269da313c2e0dd
74sharlock <sharlock7788@gmail.com>
2017-12-19 11:30:51 +0800

    add helper [utils] new method: isEmpty;

--- src/scripts/helpers/utils.js
+++ src/scripts/helpers/utils.js
@@ -1,5 +1,35 @@
 const jsonEqual = (a, b) => JSON.stringify(a) === JSON.stringify(b); // The ORDER of the properties IS IMPORTANT
 
+const isEmpty = _var => {
+  let type = getType(_var);
+  let conditions = {
+    string () {
+      return _var === '' || _var === '0';
+    },
+    object () {
+      return Object.keys(_var).length === 0;
+    },
+    array () {
+      return _var.length === 0;
+    },
+    boolean () {
+      return _var === false;
+    },
+    undefined () {
+      return true;
+    },
+    number () {
+      return _var === 0;
+    },
+    null () {
+      return true;
+    }
+  };
+
+  return conditions[type] ? conditions[type]() : false;
+};
+
 export {
-  jsonEqual
+  jsonEqual,
+  isEmpty
 };
