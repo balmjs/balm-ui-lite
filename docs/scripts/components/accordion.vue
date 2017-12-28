7013c116df4b0a09e5b5259bf375ed71a5e35e9c
74sharlock <sharlock7788@gmail.com>
2017-12-28 10:59:05 +0800

    update docs component.

--- docs/scripts/components/accordion.vue
+++ docs/scripts/components/accordion.vue
@@ -3,8 +3,14 @@
     <div class="toggle-button-container" @click="toggle">
       <slot name="toggle-button">
         <ui-button>
-          <ui-icon size="16">code</ui-icon>
-          {{ show ? '收起代码' : '显示代码' }}
+          <template v-if="show">
+            <ui-icon size="16">code</ui-icon>
+            收起代码
+          </template>
+          <template v-else>
+            <ui-icon size="16">settings_ethernet</ui-icon>
+            显示代码
+          </template>
         </ui-button>
       </slot>
     </div>
