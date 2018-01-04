7826a1469b0d99d5e49498b01beac5597ea17c39
elf-mouse <ifmouz@gmail.com>
2018-01-04 11:29:31 +0800

    build 1.0.4

--- src/scripts/mixins/mdl.js
+++ src/scripts/mixins/mdl.js
@@ -1,7 +1,8 @@
 import '../../material-design-lite/mdlComponentHandler';
 
 // Just for node (2018.01.04 by Elf-mousE)
-if (typeof exports === 'object') {
+const isNode = Object.prototype.toString.call(typeof global['process'] !== 'undefined' ? global['process'] : 0) === '[object process]';
+if (isNode && global['process'].env.NODE_ENV === 'test') {
   global.componentHandler = window.componentHandler;
 }
 
