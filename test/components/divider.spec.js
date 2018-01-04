0409d2fa9e271d16d114bff9f2c734c9fe1e8106
74sharlock <sharlock7788@gmail.com>
2018-01-04 10:42:47 +0800

    add components [divider] unit test.

---
+++ test/components/divider.spec.js
@@ -0,0 +1,62 @@
+import UiDivider from '../../components/divider';
+
+describe('<ui-divider>', () => {
+  it('默认渲染一个水平的分割线', () => {
+    let wrapper = mount(UiDivider, {
+      slots: {
+        default: 'diviiiiider!'
+      }
+    });
+    let el = wrapper.vm.$el;
+    expect(el.innerHTML).to.equal('<span>diviiiiider!</span>');
+    expect(el.classList.contains('horizontal')).to.equal(true);
+  });
+
+  it(`type为'horizontal'或'h'或0时，应该渲染为一个水平分割线`, () => {
+    let wrapper1 = mount(UiDivider, {
+      propsData: {
+        type: 'horizontal'
+      }
+    });
+    let wrapper2 = mount(UiDivider, {
+      propsData: {
+        type: 'h'
+      }
+    });
+    let wrapper3 = mount(UiDivider, {
+      propsData: {
+        type: 0
+      }
+    });
+
+    let result = wrapper1.vm.$el.classList.contains('horizontal') &&
+      wrapper2.vm.$el.classList.contains('horizontal') &&
+      wrapper3.vm.$el.classList.contains('horizontal');
+
+    expect(result).to.equal(true);
+  });
+
+  it(`type为'vertical'或'v'或1时，应该渲染为一个垂直分割线`, () => {
+    let wrapper1 = mount(UiDivider, {
+      propsData: {
+        type: 'vertical'
+      }
+    });
+    let wrapper2 = mount(UiDivider, {
+      propsData: {
+        type: 'v'
+      }
+    });
+    let wrapper3 = mount(UiDivider, {
+      propsData: {
+        type: 1
+      }
+    });
+
+    let result = wrapper1.vm.$el.classList.contains('vertical') &&
+      wrapper2.vm.$el.classList.contains('vertical') &&
+      wrapper3.vm.$el.classList.contains('vertical');
+
+    expect(result).to.equal(true);
+  })
+});
