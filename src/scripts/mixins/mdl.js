9f2fa80f570296345cc09199e17c727340f4ec66
elf-mouse <ifmouz@gmail.com>
2018-01-04 17:00:47 +0800

    1.0.5

--- src/scripts/mixins/mdl.js
+++ src/scripts/mixins/mdl.js
@@ -2,19 +2,23 @@ import '../../material-design-lite/mdlComponentHandler';
 
 // Just for node (2018.01.04 by Elf-mousE)
 const isNode = Object.prototype.toString.call(typeof global['process'] !== 'undefined' ? global['process'] : 0) === '[object process]';
-if (isNode && global['process'].env.NODE_ENV === 'test') {
-  global.componentHandler = window.componentHandler;
+const isTest = isNode && global['process'].env.NODE_ENV === 'test';
+
+let MDL = window.componentHandler;
+if (isTest) {
+  global.componentHandler = MDL;
+  MDL = global.componentHandler;
 }
 
 export default {
   data() {
     return {
-      $mdl: window.componentHandler
+      $mdl: MDL
     };
   },
   created() {
     if (!this.$mdl) {
-      this.$mdl = window.componentHandler;
+      this.$mdl = MDL;
     }
   }
 };
