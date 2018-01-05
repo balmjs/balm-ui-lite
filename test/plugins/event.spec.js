50e772f69e86b5136b750ff3843e5548bf91ec58
74sharlock <sharlock7788@gmail.com>
2018-01-05 14:26:31 +0800

    add helper [is] unit test.

--- test/plugins/event.spec.js
+++ test/plugins/event.spec.js
@@ -1,24 +1,40 @@
 import { createLocalVue } from 'vue-test-utils';
 import $event from '../../plugins/event';
-import sinon from 'sinon';
 
 let localVue = createLocalVue();
 
-let createdScene = () => {
-  return {
-    template: `<input type="text" :value="formData.name" @change="balmUI.onChange('formData.name', $event.target.value, callback)">`,
+let createdScene = (obj) => {
+  return Object.assign({}, {
+    template: `<div>
+                <input type="text" :value="formData.name" @change="balmUI.onChange('formData.name', $event.target.value, callback)">
+              </div>`,
     data () {
       return {
         formData: {
-          name: ''
+          name: '',
+          show: false
         }
       };
     },
     methods: {
       callback () {}
     }
-  };
+  }, obj);
 };
+
+let createTester = () => createdScene({
+  template: `<div>1<tester 
+                      @close="balmUI.onClose('formData.show', callback)" 
+                      @show="balmUI.onShow('formData.show', callback)"></tester></div>`,
+  components: {
+    tester: {
+      template: `<div class="tester">
+                    <span class="close-btn" @click="$emit('close')">1</span>
+                    <span class="show-btn" @click="$emit('show')">2</span>
+                   </div>`
+    }
+  }
+});
 
 localVue.use($event);
 
@@ -53,7 +69,58 @@ describe('$event', () => {
 
     expect(wrapper.vm.formData.name).to.equal('newName');
 
-    expect(callback.calledOnce).to.equal(true);
+    sinon.assert.calledOnce(callback);
   });
+
+  /** TODO: waiting [mount] for child component render bug fixed
+   * https://github.com/vuejs/vue-test-utils/issues/329
+   **/
+
+  /*it('balmUI.onShow: 自动更新指定属性状态为true，并执行一次方法', () => {
+
+    let callback = sinon.spy();
+
+    let scene = createTester();
+
+    console.log(scene);
+
+    let wrapper = mount({
+      template: `<div>1<tester></tester></div>`,
+      components: {
+        tester: {
+          template: `<div class="tester">test</div>`
+        }
+      }
+    });
+
+    console.log(wrapper.html());
+
+    let btn = wrapper.find('.show-btn');
+
+    btn.trigger('click');
+
+    expect(wrapper.vm.formData.show).to.equal(true);
+
+    sinon.assert.calledOnce(callback);
+  });
+
+  it('balmUI.onClose: 自动更新指定属性状态为false，并执行一次方法', () => {
+
+    let callback = sinon.spy();
+
+    let wrapper = mount(createTester(), {
+      localVue,
+      methods: { callback }
+    });
+
+    let btn = wrapper.find('.close-btn');
+
+    btn.trigger('click');
+
+    expect(wrapper.vm.formData.show).to.equal(false);
+
+    sinon.assert.calledOnce(callback);
+  });*/
+
 });
 
