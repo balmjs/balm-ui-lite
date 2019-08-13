2c790b6d32a82456eb79e9bfe53fef0f2be7889b
Elf-mousE <ifmouz@gmail.com>
2019-08-13 23:52:17 +0800

    update config

--- babel.config.js
+++ babel.config.js
@@ -1,42 +1,48 @@
-module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
+module.exports = function(api) {
+  let envOptions = api.env('production')
+    ? {}
+    : {
         modules: false,
         useBuiltIns: 'entry',
         corejs: 2
-      }
-    ]
-  ],
-  plugins: [
-    // Stage 2
-    [
-      '@babel/plugin-proposal-decorators',
-      {
-        legacy: true
-      }
-    ],
-    '@babel/plugin-proposal-function-sent',
-    '@babel/plugin-proposal-export-namespace-from',
-    '@babel/plugin-proposal-numeric-separator',
-    '@babel/plugin-proposal-throw-expressions',
-    // Stage 3
-    '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-syntax-import-meta',
-    [
-      '@babel/plugin-proposal-class-properties',
-      {
-        loose: false
-      }
-    ],
-    '@babel/plugin-proposal-json-strings',
-    // Runtime
-    [
-      '@babel/plugin-transform-runtime',
-      {
+      };
+  let runtimeOptions = api.env('production')
+    ? {}
+    : {
         corejs: 2
+      };
+
+  return {
+    presets: [['@babel/preset-env', envOptions]],
+    plugins: [
+      // Stage 2
+      [
+        '@babel/plugin-proposal-decorators',
+        {
+          legacy: true
+        }
+      ],
+      '@babel/plugin-proposal-function-sent',
+      '@babel/plugin-proposal-export-namespace-from',
+      '@babel/plugin-proposal-numeric-separator',
+      '@babel/plugin-proposal-throw-expressions',
+      // Stage 3
+      '@babel/plugin-syntax-dynamic-import',
+      '@babel/plugin-syntax-import-meta',
+      [
+        '@babel/plugin-proposal-class-properties',
+        {
+          loose: false
+        }
+      ],
+      '@babel/plugin-proposal-json-strings',
+      // Runtime
+      ['@babel/plugin-transform-runtime', runtimeOptions]
+    ],
+    env: {
+      test: {
+        presets: ['@babel/preset-env']
       }
-    ]
-  ]
+    }
+  };
 };
