60a9de97011b8eca908b0ced3f1ae0114c8c23c4
Elf-mousE <ifmouz@gmail.com>
2019-08-22 17:39:31 +0800

    update docs css for ie

--- docs/scripts/polyfills/index.js
+++ docs/scripts/polyfills/index.js
@@ -1,12 +1,11 @@
-import 'core-js';
-
-// for IE9-
-import 'classlist.js';
-import 'matchmedia-polyfill';
-import 'matchmedia-polyfill/matchMedia.addListener';
-import './rAF';
+import 'core-js/es6';
 
 // fuck IE
 if (typeof NodeList.prototype.forEach !== typeof alert) {
   NodeList.prototype.forEach = Array.prototype.forEach;
 }
+
+// for IE9-
+import 'matchmedia-polyfill';
+import 'matchmedia-polyfill/matchMedia.addListener';
+import './rAF';
